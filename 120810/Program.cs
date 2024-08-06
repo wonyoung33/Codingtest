ec3a5e770bcabc14c099ede3cfd80a3a33b08021
wonyoung33 <wy090801@naver.com>
2024-08-06 17:45:40 +0900

    120810
    나머지 구하기

---
+++ 120810/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace 나머지_구하기_120810
+{
+    // 문제 : 나머지 구하기120810
+    // 시작날짜 : 2024,08,05 아직 못푼산태 하나틀리고 하나맞음
+    // 문제설명 : 정수 num1,num2가 매개변수로 주어질 때,num1를num2로 나눈 나머지를 
+    // return하도록 solution함수를 완성해주세요.
+
+    // 입출력예시 #1
+    // num1이3, num2가 2이므로 3을 2로 나눈 나머지 1을  return합니다.
+    // 입출력예시 #2
+    // num1이10 ,num2가 5이므로 10을 5로 나눈 나머지 0을  return합니다.
+
+    public class Solution
+    {
+        public int solution(int num1, int num2)
+        {
+
+            return num1 % num2;
+        }
+    }
+
+}
