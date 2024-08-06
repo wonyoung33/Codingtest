15e377b6a1dcca27be975428984d7af3a5c9e72e
wonyoung33 <wy090801@naver.com>
2024-08-06 18:49:13 +0900

    120802
    두 수의 합

---
+++ 120802/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _120802
+{
+    // 문제 : 두수의 합
+    // 시작날짜 : 2024,08,06 
+    //문제설명 : 정수 num1과 num2가 주어질 때, num1과 num2의
+    //합을 return하도록 soltuion 함수를 완성해주세요.
+
+    // 입출력예시 #1
+    // num1이 2이고 num2가 3이므로 2 + 3 = 5를 return합니다.
+    // 입출력예시 #2
+    // num1이 100이고 num2가 2이므로 100 + 2 = 102를 return합니다.
+
+    // 풀이 : int(자료형)에 num1 과 num2를 받아준다.
+    // 받은 num1, num2를 + 를 통해 더해준다.
+    public class Solution
+    {
+        public int solution(int num1, int num2)
+        {
+
+            return num1 + num2;
+        }
+    }
+}
