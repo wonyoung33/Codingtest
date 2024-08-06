a8902e88bbf190cf28eb4f2712ce01640623fb3d
wonyoung33 <wy090801@naver.com>
2024-08-06 16:29:43 +0900

    120804
    두 수의 곱

---
+++ 120804/120804.cs
@@ -0,0 +1,21 @@
+﻿namespace _120804_두_수의_곱
+{
+    // 문제 :    두 수의 곱120804
+    // 시작날짜 : 2024,08,05
+    // 문제설명 : 정수 num1, num2가 매개변수 주어집니다. num1과 num2를
+    // 곱한 값을 return 하도록 solution 함수를 완성해주세요.
+
+    // 풀이 : 클래스에 Soiution이라는 함수를 만들어주고
+    // int라는 자료형을 통해 num1 과 num2를 넣어준다
+    // 그리고 그 곱한값을 return으로 표현해준다.
+
+
+    public class Solution
+    {
+        public int solution(int num1, int num2)
+        {
+            int answer = 0;
+            return num1 * num2;
+        }
+    }
+}
